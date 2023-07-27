feb1d4a55b926fbc43d9f275131e04617d257dbe
TimalkaK <timalkamk@gmail.com>
2023-07-27 17:41:27 +0100

    Feat: Added code to account for 0 and more than 3000

--- roman-numerals.js
+++ roman-numerals.js
@@ -5,13 +5,14 @@ export function convertToRomanNumerals(num) {
  const romanConstants = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"};
 
  if (num in romanConstants){
-  console.log(`${num} is in object and the numeral is ${romanConstants[num]}`);
+  //console.log(`${num} is in object and the numeral is ${romanConstants[num]}`);
   return romanConstants[num];
+ }else if (num === 0 | num > 3000){
+  //console.log(`The number ${num} is undefined`)
+  return undefined;
+ }else{
+  console.log(num); 
  }
-
-
-
-  //return "V";
    
 }
 
--- test/roman-numerals.test.js
+++ test/roman-numerals.test.js
@@ -1,8 +1,19 @@
 import { convertToRomanNumerals } from "../roman-numerals.js";
 
 describe("convertToRomanNumerals", () => {
-  test("returns the number converted to Roman Numerals", () => {
-    //expect(convertToRomanNumerals()).toBe("V");
+  test("returns the constant roman numbers converted to Roman Numerals", () => {
+    expect(convertToRomanNumerals(1)).toBe("I");
     expect(convertToRomanNumerals(5)).toBe("V");
+    expect(convertToRomanNumerals(10)).toBe("X");
+    expect(convertToRomanNumerals(50)).toBe("L");
+    expect(convertToRomanNumerals(100)).toBe("C");
+    expect(convertToRomanNumerals(500)).toBe("D");
+    expect(convertToRomanNumerals(1000)).toBe("M");
+  });
+
+  test("returns undefined for number 0 and more than 3000", () => {
+    expect(convertToRomanNumerals(0)).toBe(undefined);
+    expect(convertToRomanNumerals(3001)).toBe(undefined);
+    expect(convertToRomanNumerals(4000)).toBe(undefined);
   });
 });

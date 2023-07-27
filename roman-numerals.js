a47adbb40ad3b882a4777de929d0e0f1416a7be0
TimalkaK <timalkamk@gmail.com>
2023-07-27 17:27:42 +0100

    Feat: Roman Numerals are returning

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,3 +1,17 @@
-export function convertToRomanNumerals() {
-  return "V";
+export function convertToRomanNumerals(num) {
+ //console.log(`${num}` + "V");
+
+ // the main roman numerals 
+ const romanConstants = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"};
+
+ if (num in romanConstants){
+  console.log(`${num} is in object and the numeral is ${romanConstants[num]}`);
+  return romanConstants[num];
+ }
+
+
+
+  //return "V";
+   
 }
+
--- test/roman-numerals.test.js
+++ test/roman-numerals.test.js
@@ -2,6 +2,7 @@ import { convertToRomanNumerals } from "../roman-numerals.js";
 
 describe("convertToRomanNumerals", () => {
   test("returns the number converted to Roman Numerals", () => {
-    expect(convertToRomanNumerals()).toBe("V");
+    //expect(convertToRomanNumerals()).toBe("V");
+    expect(convertToRomanNumerals(5)).toBe("V");
   });
 });
